47c53cfd4cad6bd442e7897129c94250e787c358
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-01-01 15:52:07 +0900

    Kadai14

--- mela/Kadai14/Src/main.c
+++ mela/Kadai14/Src/main.c
@@ -111,10 +111,12 @@ int main(void)
     HAL_ADC_Start(&hadc1);
     HAL_ADC_PollForConversion(&hadc1, 10);
     if (HAL_ADC_GetState(&hadc1) & HAL_ADC_STATE_EOC_REG) {
-      htim3.Instance -> CCR1 = HAL_ADC_GetValue(&hadc1)*10;
-      HAL_Delay(100);
+      htim3.Instance -> CCR2 = HAL_ADC_GetValue(&hadc1)*10;
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
     }
     HAL_ADC_Stop(&hadc1);
+    
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
