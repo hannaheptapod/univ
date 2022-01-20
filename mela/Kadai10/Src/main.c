feb4aa162f9da85169454739c6dbc383367f0ab6
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-01-20 15:45:45 +0900

    Kadai10

--- mela/Kadai10/Src/main.c
+++ mela/Kadai10/Src/main.c
@@ -63,14 +63,14 @@ static void MX_TIM2_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef hadc) {
-  HAL_ADC_Start(&hadc1);
-  HAL_ADC_PollForConversion(&hadc1, 10);
-  if (HAL_ADC_GetState(&hadc1) & HAL_ADC_STATE_EOC_REG) {
-    htim2.Instance -> CCR1 = HAL_ADC_GetValue(&hadc1)*10;
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
+  HAL_ADC_Start(&hadc);
+  HAL_ADC_PollForConversion(&hadc, 10);
+  if (HAL_ADC_GetState(&hadc) & HAL_ADC_STATE_EOC_REG) {
+    htim2.Instance -> CCR1 = HAL_ADC_GetValue(&hadc)*10;
     HAL_Delay(100);
   }
-  HAL_ADC_Stop(&hadc1);
+  HAL_ADC_Stop(&hadc);
 }
 /* USER CODE END 0 */
 
