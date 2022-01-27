bfd46ac33a98896dfd3b23fe22316b5fb6db719c
Hannah <81697444+hannaheptapod@users.noreply.github.com>
2022-01-27 18:23:01 +0900

    KadaiFinal

--- mela/KadaiFinal/Src/main.c
+++ mela/KadaiFinal/Src/main.c
@@ -43,7 +43,7 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+uint16_t cnt = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -56,7 +56,9 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+  cnt++;
+}
 /* USER CODE END 0 */
 
 /**
